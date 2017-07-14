9b7d61bf229fc79284791a814f9339f654b454af
Celso Mireles <celso.mireles@gmail.com>
2017-07-13 20:53:24 -0700

    Starts out directory scaffolding

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './app/containers/home/Home';
 
-export default class App extends React.Component {
+export default class Cosecha extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Home />
       </View>
     );
   }
